d4b1f91c5f1ab69fb7a88c4ab3751ba63607b259
John Glen Siy <johnglen_siy@berkeley.edu>
2025-09-30 19:27:08 -0700

    refactor: add types files for organization

---
+++ src/player/types.ts
@@ -0,0 +1,11 @@
+export interface Player {
+    id: number
+    firstName: string
+    lastName: string
+    skill: number
+    wins: number
+    losses: number
+    country: string
+    seed: number
+    isSeeded: boolean
+}
